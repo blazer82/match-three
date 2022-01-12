58cb38150feb8dda2b94b022b91ca822f4458901
Raphael Stäbler <raphael@productionbuild.de>
2022-01-12 17:33:27 +0100

    Add app center script for ios

---
+++ ios/appcenter-post-clone.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+cd ..
+git clone -b 2.8.1 https://github.com/flutter/flutter.git
+export PATH=`pwd`/flutter/bin:$PATH
+
+flutter doctor
+
+echo "Installed flutter to `pwd`/flutter"
+
+flutter packages pub get
+
+flutter build ios --release --no-codesign
